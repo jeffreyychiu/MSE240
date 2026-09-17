fd0a48d66c7cff4aee7b32dad7f13946420c2f1a
Jeffrey Chiu <jeffrey.chiu227@gmail.com>
2026-09-17 15:31:57 -0400

    Sept 17

---
+++ lec003.py
@@ -0,0 +1,53 @@
+class Dog:
+
+    def __init__(self):
+        raise NotImplementedError("Not implemented yet")
+
+"" should have:
+
+#_breed: str
+#_age: int
+#_name: str
+#_Weight: float
+
+#_last_fed_time: Datetime
+#_last_walk_time: Datetime
+
+#accessor
+# is_fed(): bool
+# is_walked(): bool
+#_is_healthy_weight(): bool
+
+
+#mutator 
+# feed()
+# walk()
+# set_weight(new_weight, in_kg)
+
+
+""
+
+
+def __init__(self, 
+             breed: str, 
+             age: int, 
+             name: str, 
+             weight: float):
+    ""
+    Constructor for Dog class
+
+    Parameters
+    name: Name of the dog
+    breed: Breed of the dog
+    age: Age of the dog
+    weight: Weight of the dog in kg
+    ""
+    raise NotImplementedError("Not implemented yet")
+
+
+
+
+#Main Function
+if__name__ == "__main__":
+    waffle = Dog()
+    jazz = Dog()
